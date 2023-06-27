6136090e4ee9b5cf1b683ce4f3b3eb4480d2ba72
Sarunas <sverunas@gmail.com>
2023-06-27 11:13:47 +0300

    backas

---
+++ Back/servisas/src/main/java/com/auto/servisas/ServisasApplication.java
@@ -0,0 +1,13 @@
+package com.auto.servisas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServisasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServisasApplication.class, args);
+	}
+
+}
